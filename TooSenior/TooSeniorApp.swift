a49125cdbffb91ee66428a3b87ee73e74c97b313
Vladyslav Krut <krut.vladyslav@gmail.com>
2025-07-25 17:33:48 +0200

    Migrate the app to SwiftUI

---
+++ TooSenior/TooSeniorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooSeniorApp.swift
+//  TooSenior
+//
+//  Created by Vladyslav Krut on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TooSeniorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AuthenticationView()
+        }
+    }
+}
